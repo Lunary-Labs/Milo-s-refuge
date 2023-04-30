baf448a397ca98dbc9b4cea3e34221241a2b726c
LoicLemai <loiclemai.pro@gmail.com>
2023-04-30 21:52:22 +0200

    wheat prefab
    Render and animation of wheat growing in time

--- Assets/Scripts/field_tile.cs
+++ Assets/Scripts/field_tile.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class field_tile : MonoBehaviour
 {
-    public float growth_state = 0f;
+    public int growth_state = 0;
     private float next_growth_timer = 0f;
     private float growth_timer = 0f;
 
-    private float max_growth = 10f;
+    private int max_growth = 3;
     public bool harvestable = false;
 
-    void start() {
+    public Sprite[] sprites;
+    private SpriteRenderer spriteRenderer;
+
+    void Start() {
         next_growth_timer = Random.Range(0f, 1f);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        spriteRenderer.sprite = sprites[growth_state];
     }
 
     public void grow(float time) {
         if (!harvestable) {
             growth_timer += time;
             if (growth_timer >= next_growth_timer) {
-                growth_state += 1f;
+                growth_state += 1;
                 growth_timer = 0f;
                 next_growth_timer = Random.Range(0f, 1f);
                 if (growth_state >= max_growth) {
@@ -31,7 +39,7 @@ public class field_tile : MonoBehaviour
     }
 
     public void harvest() {
-        growth_state = 0f;
+        growth_state = 0;
         harvestable = false;
         next_growth_timer = Random.Range(0f, 1f);
     }
