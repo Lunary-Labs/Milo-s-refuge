222649adc7cd1faa7a6ae0e648f38fa7e2d705aa
LoicLemai <loiclemai.pro@gmail.com>
2023-07-14 20:12:35 +0200

    progress bar fix
    progress bar fixed

--- Assets/Scripts/progress_bar_cooker.cs
+++ Assets/Scripts/progress_bar_cooker.cs
@@ -7,22 +7,24 @@ public class progress_bar_cooker : MonoBehaviour
 {
     public GameObject image_component;
     public GameObject game_manager;
-    public cooker cooker_object;
+    public cooker cooker_script;
     public Image progress_bar;
+    private int current_cooker;
     // Start is called before the first frame update
     void Start()
     {
         progress_bar = image_component.GetComponent<Image>();
         game_manager = GameObject.Find("game_manager");
-        cooker_object = game_manager.GetComponent<cooker>();
+        cooker_script = game_manager.GetComponent<cooker>();
+        current_cooker = int.Parse(transform.parent.parent.name);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(cooker_object.cooker_1_started){
-            float timer = (float)cooker_object.recipe_dict[cooker_object.recipe_cooker_1].duration;
-            progress_bar.fillAmount = cooker_object.timer_cooker_1/timer;
+        if(cooker_script.cookers[current_cooker].started){
+            float timer = (float)cooker_script.recipe_dict[cooker_script.cookers[current_cooker].recipe].duration;
+            progress_bar.fillAmount = cooker_script.cookers[current_cooker].timer/timer;
         }
         else{
             progress_bar.fillAmount = 0;
