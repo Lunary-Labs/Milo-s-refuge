1f1b554949a11794e02869cb009ddac2d0565cca
Loïc Pottier <loic.pottier@live.fr>
2024-06-12 15:21:02 +0200

    feat: handle basic weather state switch and rain particles

--- Assets/Scripts/Weather/WeatherController.cs
+++ Assets/Scripts/Weather/WeatherController.cs
@@ -3,28 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WeatherController : MonoBehaviour {
-  public string CurrentTime; // TODO: Remove this display attribute in the future
-  [SerializeField] public int DayCounter = 0;
+  public int DayCounter = 0;
   public float Percentage = 0f;
+  public string CurrentWeather = "Neutral";
+
   private float _timeElapsed = 0f;
-  private float _duration = 100f; // Will be 1440 (1sec RT = 1 min IGT)
+  private float _dayDuration = 48f; // Will be 1440 (1sec RT = 1 min IGT)
+
+  private float _weatherDurationMin = 10; // Will be 120f
+  private float _weatherDurationMax = 20; // Will be 300f
+  public float _timeElapsedWeather = 0f;
+  public float _nextWeatherDelay;
+  private List<WeatherState> _weatherStates;
+
+  public GameObject _weatherParticles;
+  public ParticleSystem _particleSystem;
+
+  void Start() {
+    _weatherParticles = GameObject.Find("Main Camera").transform.Find("WeatherParticles").gameObject;
+    _particleSystem =_weatherParticles.GetComponent<ParticleSystem>();
+    _weatherStates = new List<WeatherState>() {
+      new WeatherState("Neutral", 0.68f),
+      new WeatherState("Rain", 0.08f),
+      new WeatherState("Clouds", 0.08f),
+      new WeatherState("Fog", 0.08f),
+      new WeatherState("Storm", 0.08f)
+    };
+    CurrentWeather = "Neutral";
+  }
 
   void Update() {
     _timeElapsed += Time.deltaTime;
-    if (_timeElapsed >= _duration) {
+    _timeElapsedWeather += Time.deltaTime;
+    if(_timeElapsed >= _dayDuration) {
       DayCounter++;
       _timeElapsed = 0;
     }
-    Percentage = _timeElapsed / _duration;
-    UpdateCurrentTime(Percentage);
+    Percentage = _timeElapsed / _dayDuration;
+
+    if(_timeElapsedWeather >= _nextWeatherDelay) {
+      float randomVal = Random.Range(0f, 1f);
+      float totalWeight = 0f;
+      foreach(WeatherState state in _weatherStates) {
+        totalWeight += state.Weight;
+        if(randomVal < totalWeight) {
+          CurrentWeather = state.Name;
+          switch (CurrentWeather) {
+            // TODO Make transitions on particle system instead of just active/inactive
+            case "Rain":
+              _weatherParticles.SetActive(true);
+              // TODO start rain ambient sound
+              // TODO color change
+              break;
+            case "Storm":
+              _weatherParticles.SetActive(true);
+              // TODO start storm ambient sounds
+              // TODO color change
+              break;
+            case "Clouds":
+              _weatherParticles.SetActive(false);
+              // TODO maybe another particle system
+              // TODO color change
+              break;
+            case "Fog":
+              _weatherParticles.SetActive(false);
+              // TODO maybe another particle system
+              // TODO color change
+              break;
+            default:
+              _weatherParticles.SetActive(false);
+              break;
+          }
+          break;
+        }
+      }
+      _timeElapsedWeather = 0f;
+      _nextWeatherDelay = Random.Range(_weatherDurationMin, _weatherDurationMax);
+    }
   }
 
-  // TODO: Remove this temp function in the future
-  private void UpdateCurrentTime(float percentage) {
-    float totalMinutesInDay = 24 * 60;
-    float currentMinutes = percentage * totalMinutesInDay;
-    int hours = Mathf.FloorToInt(currentMinutes / 60);
-    int minutes = Mathf.FloorToInt(currentMinutes % 60);
-    CurrentTime = string.Format("{00:D2}:{01:D2}", hours, minutes);
+  private class WeatherState {
+    public string Name;
+    public float Weight;
+    public WeatherState(string name, float weight) {
+      Name = name;
+      Weight = weight;
+    }
   }
 }
